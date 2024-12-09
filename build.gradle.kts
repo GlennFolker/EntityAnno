3c4deb3ec289232c65fdd0c18d24123522f37056
GlennFolker <glenn.folk08@gmail.com>
2024-12-09 17:46:31 +0700

    More information on POM

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.gradle.api.publish.maven.internal.publication.*
 import java.io.*
 
 plugins{
@@ -26,11 +27,32 @@ fun kotlinPlugin(module: String): String{
     return "org.jetbrains.kotlin.$module:org.jetbrains.kotlin.$module.gradle.plugin:$kotlinVersion"
 }
 
+fun commonPom(pom: MavenPom){
+    pom.apply{
+        url = "https://github.com/GlennFolker/EntityAnno"
+        inceptionYear = "2024"
+
+        licenses{
+            license{
+                name = "GPL-3.0-or-later"
+                url = "https://www.gnu.org/licenses/gpl-3.0.en.html"
+                distribution = "repo"
+            }
+        }
+
+        issueManagement{
+            system = "GitHub Issue Tracker"
+            url = "https://github.com/GlennFolker/EntityAnno/issues"
+        }
+    }
+}
+
 allprojects{
     apply(plugin = "java")
     apply(plugin = "maven-publish")
 
     sourceSets["main"].java.setSrcDirs(listOf(layout.projectDirectory.dir("src")))
+    group = "com.github.GlennFolker.EntityAnno"
 
     repositories{
         google()
@@ -86,18 +108,34 @@ configure(allprojects - project(":downgrader")){
 
 configure(listOf(project(":downgrader"), project(":entity"))){
     sourceSets["main"].resources.setSrcDirs(listOf(layout.projectDirectory.dir("assets")))
-
-    group = "com.github.GlennFolker.EntityAnno"
-    publishing.publications.register<MavenPublication>("maven"){
-        from(components["java"])
-    }
-
     dependencies{
         implementation(arc(":arc-core"))
     }
 }
 
+project(":downgrader"){
+    publishing.publications.register<MavenPublication>("maven"){
+        from(components["java"])
+        pom{
+            name = "EntityAnno Syntax Downgrader"
+            description = "Java 9+ syntax availability in Java 8, which is necessary for Mindustry mods."
+            commonPom(this)
+        }
+    }
+}
+
 project(":entity"){
+    publishing.publications.register<MavenPublication>("maven"){
+        from(components["java"])
+        pom{
+            name = "EntityAnno Annotation Processor"
+            description = "Nearly one-to-one integration of the entity component class generator in Mindustry with " +
+                    "an additional utility to properly register the entity class IDs for usage in `UnitType`s and " +
+                    "persistence across save files."
+            commonPom(this)
+        }
+    }
+
     dependencies{
         implementation(mindustry(":core"))
         implementation(javapoet())
@@ -108,11 +146,58 @@ project(":"){
     apply(plugin = "java-gradle-plugin")
 
     group = "com.github.GlennFolker"
-    configure<GradlePluginDevelopmentExtension>{
-        plugins.register("entityAnno"){
+
+    lateinit var plugin: Provider<PluginDeclaration>
+    gradlePlugin{
+        isAutomatedPublishing = false
+
+        plugin = plugins.register("entityAnno"){
             id = "com.github.GlennFolker.EntityAnno"
             displayName = "EntityAnno"
+            description = "Utility tools for generating Mindustry custom entity component classes."
             implementationClass = "ent.EntityAnnoPlugin"
+        }
+    }
+
+    publishing.publications{
+        fun applyPom(pom: MavenPom){
+            pom.apply{
+                name = plugin.map{it.displayName}
+                description = plugin.map{it.description}
+                commonPom(this)
+            }
+        }
+
+        val maven = register<MavenPublication>("maven"){
+            from(components["java"])
+            pom{applyPom(this)}
+        }
+
+        register<MavenPublication>("plugin"){
+            (this as MavenPublicationInternal).isAlias = true
+            groupId = plugin.map{it.id}.get()
+            artifactId = "$groupId.gradle.plugin"
+
+            pom{
+                packaging = "pom"
+
+                applyPom(this)
+                withXml{
+                    val root = asElement()
+                    val doc = root.ownerDocument
+                    val dependencies = root.appendChild(doc.createElement("dependencies"))
+                    val dependency = dependencies.appendChild(doc.createElement("dependency"))
+
+                    val groupId = dependency.appendChild(doc.createElement("groupId"))
+                    groupId.textContent = maven.map{it.groupId}.get()
+
+                    val artifactId = dependency.appendChild(doc.createElement("artifactId"))
+                    artifactId.textContent = maven.map{it.artifactId}.get()
+
+                    val version = dependency.appendChild(doc.createElement("version"))
+                    version.textContent = maven.map{it.version}.get()
+                }
+            }
         }
     }
 
